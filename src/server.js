2c424947537ca416a4729c6f041f4180969ae47e
yawen xue <124210098+yawenx2004@users.noreply.github.com>
2025-06-20 19:56:57 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from backend!! :)');
+});
+
+// in-memory store for location data
+// to be replaced with DB in actual app
+const locationData = {};
+
+// receive location updates
+app.post('/location', (req, res) => {
+    const { name, coords } = req.body;
+
+    if (!name || !coords) {
+        return res.status(400).json({ error: 'Missing name or coordinates' });
+    }
+
+    const entry = {
+        timestam: new Date().toISOString(),
+        coords
+    }
+
+    if (!locationData[name]) {
+        locationData[name] = [];
+    }
+
+    locationData[name].push(entry);
+
+    console.log(`Logged location for ${name}:`, entry);
+
+    res.status(200).json({ message: 'Location saved successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}...`);
+});
